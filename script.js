757b319244f7f65f5fff17c48b38331dfca158bd
KarolisGaiv <kgaivenis@gmail.com>
2022-01-17 19:11:54 +0200

    Refactor form validation - delete err msg if field is populated

--- script.js
+++ script.js
@@ -8,13 +8,11 @@ const passField = document.getElementById('password');
 const passErr = document.querySelector('.passErr');
 const form = document.querySelector('.form');
 
+let isError;
+
 form.addEventListener('submit', (e) => {
-  if (
-    fNameError.innerText ||
-    lNameError.innerText ||
-    emailErr.innerText ||
-    passErr.innerText
-  ) {
+  // Check if form has already error message
+  if (isError) {
     fNameError.innerText = '';
     lNameError.innerText = '';
     emailErr.innerText = '';
@@ -24,17 +22,21 @@ form.addEventListener('submit', (e) => {
   if (fNameField.value === '' || fNameField.value == null) {
     e.preventDefault();
     fNameError.innerText = 'First Name cannot be empty';
+    isError = true;
   }
   if (lNameField.value === '' || lNameField.value == null) {
     e.preventDefault();
     lNameError.innerText = 'Last Name cannot be empty';
+    isError = true;
   }
   if (emailField.value === '' || emailField.value == null) {
     e.preventDefault();
     emailErr.innerText = 'Looks like this is not an email';
+    isError = true;
   }
   if (passField.value === '' || passField.value == null) {
     e.preventDefault();
     passErr.innerText = 'Password cannot be empty';
+    isError = true;
   }
 });
