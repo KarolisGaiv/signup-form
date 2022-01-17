5093c919c95e60a92ef0ca9df00441abb2dd28f2
KarolisGaiv <kgaivenis@gmail.com>
2022-01-17 19:52:37 +0200

    Refactor form validation code

--- script.js
+++ script.js
@@ -8,35 +8,52 @@ const passField = document.getElementById('password');
 const passErr = document.querySelector('.passErr');
 const form = document.querySelector('.form');
 
-let isError;
+let formHasError;
 
 form.addEventListener('submit', (e) => {
+  let arr = [fNameField, lNameField, emailField, passField];
   // Check if form has already error message
-  if (isError) {
-    fNameError.innerText = '';
-    lNameError.innerText = '';
-    emailErr.innerText = '';
-    passErr.innerText = '';
+  if (formHasError) {
+    removeErrWarnings();
+    for (let i = 0; i < arr.length; i++) {
+      removeErrStyling(arr[i]);
+    }
   }
 
   if (fNameField.value === '' || fNameField.value == null) {
     e.preventDefault();
     fNameError.innerText = 'First Name cannot be empty';
-    isError = true;
+    markErr(fNameField);
   }
   if (lNameField.value === '' || lNameField.value == null) {
     e.preventDefault();
     lNameError.innerText = 'Last Name cannot be empty';
-    isError = true;
+    markErr(lNameField);
   }
   if (emailField.value === '' || emailField.value == null) {
     e.preventDefault();
     emailErr.innerText = 'Looks like this is not an email';
-    isError = true;
+    markErr(emailField);
   }
   if (passField.value === '' || passField.value == null) {
     e.preventDefault();
     passErr.innerText = 'Password cannot be empty';
-    isError = true;
+    markErr(passField);
   }
 });
+
+function markErr(field) {
+  formHasError = true;
+  field.classList.add('error');
+}
+
+function removeErrWarnings() {
+  fNameError.innerText = '';
+  lNameError.innerText = '';
+  emailErr.innerText = '';
+  passErr.innerText = '';
+}
+
+function removeErrStyling(element) {
+  element.classList.remove('error');
+}
