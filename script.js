dc2ee603d1a3d41af75b97b857bfb2f7660e7f5e
KarolisGaiv <kgaivenis@gmail.com>
2022-01-17 18:55:47 +0200

    Add initial form validation funcionality

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
-const nameField = document.getElementById('firstName');
+const fNameField = document.getElementById('firstName');
+const fNameError = document.querySelector('.fNameErr');
+const lNameField = document.getElementById('lastName');
+const lNameError = document.querySelector('.lNameErr');
+const emailField = document.getElementById('email');
+const emailErr = document.querySelector('.emailErr');
+const passField = document.getElementById('password');
+const passErr = document.querySelector('.passErr');
+const form = document.querySelector('.form');
 
-const submitBtn = document.querySelector('.submitBtn');
-
-submitBtn.addEventListener('submit', (e) => {
-  let errors = [];
-
-  if (nameField === null || nameField === '') {
-    e.preventDefault();
-
-    errors.push(error);
+form.addEventListener('submit', (e) => {
+  if (
+    fNameError.innerText ||
+    lNameError.innerText ||
+    emailErr.innerText ||
+    passErr.innerText
+  ) {
+    fNameError.innerText = '';
+    lNameError.innerText = '';
+    emailErr.innerText = '';
+    passErr.innerText = '';
   }
 
-  if (errors.length > 0) {
+  if (fNameField.value === '' || fNameField.value == null) {
     e.preventDefault();
-    console.log(errors);
+    fNameError.innerText = 'First Name cannot be empty';
+  }
+  if (lNameField.value === '' || lNameField.value == null) {
+    e.preventDefault();
+    lNameError.innerText = 'Last Name cannot be empty';
+  }
+  if (emailField.value === '' || emailField.value == null) {
+    e.preventDefault();
+    emailErr.innerText = 'Looks like this is not an email';
+  }
+  if (passField.value === '' || passField.value == null) {
+    e.preventDefault();
+    passErr.innerText = 'Password cannot be empty';
   }
 });
-
-function checkForm() {
-  console.log(test);
-}
